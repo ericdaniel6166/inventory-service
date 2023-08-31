e6bfa717613cbb2d204ed4c585e28ce7177d476d
ericdaniel6166 <64393957+ericdaniel6166@users.noreply.github.com>
2023-08-31 21:49:14 +0700

    feature/SM-0015 (#8)

--- src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example" })
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
