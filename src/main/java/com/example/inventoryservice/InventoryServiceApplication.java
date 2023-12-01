b15e8270978abbfdceec9f1da07b58c605224d5a
eric6166 <ericnguyen6166@gmail.com>
2023-12-02 00:03:37 +0700

    feature/SM-0036

--- src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example" })
 @EnableDiscoveryClient
+@EnableIntegration
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
